3fc017daadba612e9e24fbbf0fa66ac4885b4b3c
somirang <somirang777@gmail.com>
2021-06-10 13:36:11 +0900

    change

---
+++ build/precache-manifest.2e7be043c0966f57d4cba95a77ad7882.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395dbad7fcd38e4c007cffb3e53bf5d2",
+    "url": "/testtest/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "/testtest/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "39ae1c9329f68d0212fc",
+    "url": "/testtest/static/js/main.6a1521fc.chunk.js"
+  },
+  {
+    "revision": "e8114011b94919ba4c04",
+    "url": "/testtest/static/js/runtime-main.70d9eeb5.js"
+  }
+]);
