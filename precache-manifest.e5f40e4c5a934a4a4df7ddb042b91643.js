5db6d10d43d8774f9eb23f6e851e812495ba8f8c
somirang <somirang777@gmail.com>
2021-06-10 13:32:29 +0900

    Updates

---
+++ precache-manifest.e5f40e4c5a934a4a4df7ddb042b91643.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bbe3d145776567282cbcf52329dd96",
+    "url": "https//somirang.github.io/testtest/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "https//somirang.github.io/testtest/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "39ae1c9329f68d0212fc",
+    "url": "https//somirang.github.io/testtest/static/js/main.6a1521fc.chunk.js"
+  },
+  {
+    "revision": "050069b2e0188fa05f44",
+    "url": "https//somirang.github.io/testtest/static/js/runtime-main.408cbd05.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https//somirang,github.io/testtest/precache-manifest.25e893b66a4b91e1d47a2292db527144.js"
+  "https//somirang.github.io/testtest/precache-manifest.e5f40e4c5a934a4a4df7ddb042b91643.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https//somirang,github.io/testtest/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https//somirang.github.io/testtest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
