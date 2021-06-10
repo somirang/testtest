b52111fd0ea310ba7052cf8a5d58b42e530f508d
somirang <somirang777@gmail.com>
2021-06-10 13:30:51 +0900

    Updates

---
+++ precache-manifest.25e893b66a4b91e1d47a2292db527144.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57b45e181d7833017b53b2230d7c513",
+    "url": "https//somirang,github.io/testtest/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "https//somirang,github.io/testtest/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "39ae1c9329f68d0212fc",
+    "url": "https//somirang,github.io/testtest/static/js/main.6a1521fc.chunk.js"
+  },
+  {
+    "revision": "df7fd5f5efa3358beb35",
+    "url": "https//somirang,github.io/testtest/static/js/runtime-main.a14978fb.js"
+  }
+]);
